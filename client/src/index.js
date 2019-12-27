a1cb5cf23b15075b7ea660be3244cee7d606cbf4
Craig <eoschazmr@googlemail.com>
2019-12-27 05:21:09 +0000

    Homepage work started
    added some cards for games on the homepage and added required libraries

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { Provider } from 'react-redux'
-import store from './store'
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './styles/index.scss';
 import App from './pages/homepage/App';
 
 import * as serviceWorker from './serviceWorker';
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <div className='app-container'>
+      <App />
+    </div>
   </Provider>,
   rootElement
-)
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
