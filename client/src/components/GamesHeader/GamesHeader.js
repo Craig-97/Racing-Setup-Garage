f3761b5911fc27d563d862fb0d004bd1af9d653e
Craig <eoschazmr@googlemail.com>
2019-12-30 20:03:25 +0000

    Added autocomplete search and fixed styling

--- client/src/components/GamesHeader/GamesHeader.js
+++ client/src/components/GamesHeader/GamesHeader.js
@@ -1,17 +1,25 @@
 import React from 'react';
 
+import { GamesNameSearch } from '../GamesNameSearch';
 import { GamesPlatformSelect } from '../GamesPlatformSelect';
 
 import './GamesHeader.scss';
 
-export const GamesHeader = ({ BEM_BASE, updateFilteredGames }) => {
+export const GamesHeader = ({ BEM_BASE, updateFilteredGames, games }) => {
   return (
     <>
       <h4 className={`${BEM_BASE}-games__header`}>Games</h4>
-      <GamesPlatformSelect
-        BEM_BASE={BEM_BASE}
-        updateFilteredGames={updateFilteredGames}
-      />
+      <div className={`${BEM_BASE}-games__filters`}>
+        <GamesNameSearch
+          BEM_BASE={BEM_BASE}
+          updateFilteredGames={updateFilteredGames}
+          games={games}
+        />
+        <GamesPlatformSelect
+          BEM_BASE={BEM_BASE}
+          updateFilteredGames={updateFilteredGames}
+        />
+      </div>
     </>
   );
 };
