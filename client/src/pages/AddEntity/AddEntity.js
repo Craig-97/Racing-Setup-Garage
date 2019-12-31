93a231645edcbbb9463d8e98ddbdff0bfaeae582
Craig <eoschazmr@googlemail.com>
2019-12-31 03:26:41 +0000

    WIP added to new page

--- client/src/pages/AddEntity/AddEntity.js
+++ client/src/pages/AddEntity/AddEntity.js
@@ -12,8 +12,8 @@ const AddEntity = () => {
       <div className={`${BEM_BASE} page-container`}>
         <h1 className={`${BEM_BASE}-header`}>Add Entity</h1>
 
-        <h1 style={{ color: 'white', margin: '30rem 45rem' }}>
-          WORK IN PROGRESS - UNDER CONSTRUCTION .....
+        <h1 style={{ color: 'white', textAlign: 'center' }}>
+          WORK IN PROGRESS - UNDER CONSTRUCTION...
         </h1>
       </div>
     </>
