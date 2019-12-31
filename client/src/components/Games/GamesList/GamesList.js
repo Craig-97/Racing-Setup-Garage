3014f60fc9e957954da39ceceb5f77494f46ff28
Craig <eoschazmr@googlemail.com>
2019-12-31 02:46:00 +0000

    Folder changes, game card style tweaks

--- client/src/components/Games/GamesList/GamesList.js
+++ client/src/components/Games/GamesList/GamesList.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { fetchGames } from '../../api';
-import { getGames, getGamesPending } from '../../reducers/gameReducer';
-import { resultsFilterer } from '../../util'
+import { fetchGames } from '../../../api';
+import { getGames, getGamesPending } from '../../../reducers/gameReducer';
+import { resultsFilterer } from '../../../util'
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-import { GameCard } from '../../components/GameCard';
-import { GamesHeader } from '../GamesHeader/GamesHeader';
+import { GameCard } from '../GameCard'
+import { GamesHeader } from '../GamesHeader';
 
 import './GamesList.scss';
 
--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Navbar } from '../../components/Navbar';
-import { GamesList } from '../../components/GamesList';
+import { GamesList } from '../../components/Games/GamesList';
 
 import './Homepage.scss';
 
