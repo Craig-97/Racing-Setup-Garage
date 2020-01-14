cfd7e38e36bab3f25f9c4d5d343f7c36934fcc09
Craig <eoschazmr@googlemail.com>
2020-01-14 22:45:57 +0000

    DatePicker updated
    Made it fully customisable, will use default props if any are missing. Allows the passing of values from react-hooks-controller.

--- client/src/components/Controls/Datepicker/Datepicker.js
+++ client/src/components/Controls/Datepicker/Datepicker.js
@@ -3,17 +3,27 @@ import moment from 'moment';
 
 import { KeyboardDatePicker } from '@material-ui/pickers';
 
-export const Datepicker = ({ disabled }) => {
+export const Datepicker = props => {
   const [selectedDate, setSelectedDate] = useState(new moment());
+  const { disabled, placeholder, value, onChange, minDate, format } = props;
+
+  const defaults = {
+    placeholder: selectedDate.toString(),
+    value: selectedDate,
+    onChange: date => setSelectedDate(date),
+    minDate: new Date('01/01/1900'),
+    format: 'DD/MM/YYYY'
+  };
 
   return (
     <KeyboardDatePicker
       clearable
       disabled={disabled}
-      placeholder={selectedDate.toString()}
-      onChange={date => setSelectedDate(date)}
-      minDate={new Date('01/01/1900')}
-      format={'DD/MM/YYYY'}
+      placeholder={placeholder ? placeholder : defaults.placeholder}
+      value={value ? value : defaults.value}
+      onChange={onChange ? onChange : defaults.onChange}
+      minDate={minDate ? minDate : defaults.minDate}
+      format={format ? format : defaults.format}
     />
   );
 };
