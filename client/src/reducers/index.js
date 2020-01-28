6486384b02cf89ba89b69b9e101bfdb27f6aa443
Craig <eoschazmr@googlemail.com>
2020-01-28 19:36:06 +0000

    Rename Folders

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,9 +1,9 @@
 import { combineReducers } from "redux";
 import { gameReducer } from "./gameReducer";
 import { carReducer } from "./carReducer";
-import { trackReducer } from "./trackReducers";
-import { setupReducer } from "./setupReducers";
-import { driverReducer } from "./driverReducers";
+import { trackReducer } from "./trackReducer";
+import { setupReducer } from "./setupReducer";
+import { driverReducer } from "./driverReducer";
 
 export default combineReducers({
   games: gameReducer,
