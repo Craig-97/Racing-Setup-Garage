705526e138b9df3de26fae6a8abe7e2e15c47579
Craig <eoschazmr@googlemail.com>
2020-02-01 14:49:34 +0000

    fix changes from useEffect to useMemo

--- client/src/components/Management/GameManagement/GameManagement.js
+++ client/src/components/Management/GameManagement/GameManagement.js
@@ -1,13 +1,13 @@
-import React, { Fragment, useEffect, useState, useRef } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+import React, { Fragment, useEffect, useState, useRef, useMemo } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
-import MaterialTable from 'material-table';
-import { GameForm } from '../../Forms';
+import MaterialTable from "material-table";
+import { GameForm } from "../../Forms";
 
-import { fetchGames, deleteGame } from 'api';
-import { getGames, gamesCRUDPending } from 'reducers/gameReducer';
+import { fetchGames, deleteGame } from "api";
+import { getGames, gamesCRUDPending } from "reducers/gameReducer";
 
-import './GameManagement.scss';
+import "./GameManagement.scss";
 
 export const GameManagement = ({ BEM_BASE }) => {
   const dispatch = useDispatch();
@@ -44,8 +44,6 @@ export const GameManagement = ({ BEM_BASE }) => {
           game.platform = game.platform.join(", ");
         }
       });
-
-      setGameData(newGames);
     }
     return newGames;
   }, [games]);
@@ -78,7 +76,7 @@ export const GameManagement = ({ BEM_BASE }) => {
     const game = { _id, name, platform, imageURL, developer, releaseDate };
 
     if (game.platform) {
-      game.platform = game.platform.split(', ');
+      game.platform = game.platform.split(", ");
     }
     setSelectedGame(game);
   };
@@ -88,30 +86,30 @@ export const GameManagement = ({ BEM_BASE }) => {
       <GameForm
         BEM_BASE={BEM_BASE}
         selectedGame={selectedGame}
-        setSelectedGame={(value) => setSelectedGame(value)}
+        setSelectedGame={value => setSelectedGame(value)}
         showMessage={showMessage}
-        setShowMessage={(value) => setShowMessage(value)}
+        setShowMessage={value => setShowMessage(value)}
         hideMessage={hideMessage}
       />
       <div className={`${BEM_BASE}__table`}>
         <MaterialTable
           columns={[
-            { title: 'Name', field: 'name' },
-            { title: 'Platforms', field: 'platform' },
-            { title: 'Developer', field: 'developer' },
-            { title: 'Release Date', field: 'releaseDate', type: 'date' }
+            { title: "Name", field: "name" },
+            { title: "Platforms", field: "platform" },
+            { title: "Developer", field: "developer" },
+            { title: "Release Date", field: "releaseDate", type: "date" }
           ]}
           data={gameData}
-          title='Games'
+          title="Games"
           actions={[
             {
-              icon: 'edit',
-              tooltip: 'Edit Game',
+              icon: "edit",
+              tooltip: "Edit Game",
               onClick: (event, rowData) => editGame(rowData)
             },
             {
-              icon: 'delete',
-              tooltip: 'Delete Game',
+              icon: "delete",
+              tooltip: "Delete Game",
               onClick: (event, rowData) => removeGame(rowData)
             }
           ]}
